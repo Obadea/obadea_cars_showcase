ee7ad830e9638d738d17abc91b36fefff244d304
Obadiah Gbenga <124559546+Obadea@users.noreply.github.com>
2024-03-28 16:01:31 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["cdn.imagin.studio"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.imagin.studio',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
   typescript: {
     ignoreBuildErrors: true,
